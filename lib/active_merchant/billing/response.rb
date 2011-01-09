7fe5065f8ca9a12935f5690f308fd83bea95b262
Raffi Jacobs <raffij@gmail.com>
2011-01-09 17:40:06 +0000

    sage pay basics

--- lib/active_merchant/billing/response.rb
+++ lib/active_merchant/billing/response.rb
@@ -5,12 +5,16 @@ module ActiveMerchant #:nodoc:
     end
   
     class Response
-      attr_reader :params, :message, :test, :authorization, :avs_result, :cvv_result
+      attr_reader :params, :message, :test, :authorization, :avs_result, :cvv_result, :pa_req, :md, :acs_url
       
       def success?
         @success
       end
-
+      
+      def three_d_secure?
+        @three_d_secure
+      end
+      
       def test?
         @test
       end
@@ -26,6 +30,10 @@ module ActiveMerchant #:nodoc:
         @fraud_review = options[:fraud_review]
         @avs_result = AVSResult.new(options[:avs_result]).to_hash
         @cvv_result = CVVResult.new(options[:cvv_result]).to_hash
+        @three_d_secure = options[:three_d_secure]
+        @pa_req = options[:pa_req]
+        @md = options[:md]
+        @acs_url = options[:acs_url]
       end
     end
   end
